d0c04156e610e02f1204dcc63616bebf11653418
mattheusmendonca <teus2002ppbarbosa@outlook.com>
2021-07-02 05:24:28 -0300

    Explicações DIR e HELP

--- aula dir e help/dir_e_help.py
+++ aula dir e help/dir_e_help.py
@@ -1,7 +1,23 @@
 """
-dir->
+Ultilitários Python que auxiliam na programação.
 
-help->
+dir-> Apresenta todos os atributos/propriedades e funções/métodos disponíveis 
+    para determinado tipo de dados ou variáveis.
+        tipo: se eu tenho uma string "Matheus", e der um DIR, ele vai me indicar tudo que 
+        eu posso ultilizar, ex: lower, sizeof, isdecimal... Coisas que eu posso fazer com
+        essa determinada string.
+
+dir(tipo de dado/variável)
+
+help-> Apresenta a documentação/como ultilizar os atributos/propriedades e funções/métodos disponíveis 
+    para determinado tipo de dado ou variável.
+        ex: usando a determinada string acima, ai depois que eu escolher o tipo da propriedade que eu quero 
+        ultilizar, e eu nao soube muito bem o funcionamento, ou ate mesmo nao saber nada sobre o meto-
+        do, só é eu dar um HELP que ele irá me explicar como funciona...
+
+help("Matheus".lower)
 
 """
-skaljsd
+print(dir('geek'))
+help('Geek'.lower)
+
