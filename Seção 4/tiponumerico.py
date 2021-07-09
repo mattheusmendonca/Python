11c0f30c36eeea62792bdcb073db73b5d3fd7404
mattheusmendonca <teus2002ppbarbosa@outlook.com>
2021-07-09 01:04:52 -0300

    TIPOS NUMÈRICOS

---
+++ Seção 4/tiponumerico.py
@@ -0,0 +1,21 @@
+"""
+Tipo Numerico
+
+para saber se o numero é par ou impar, 
+4 % 2 = 0 
+no caso vai ser par
+5 % 2 = 1
+no caso vai ser impar 
+
+int(5/2) = 2, pois ele vai apenas exibir a parte inteira
+5 // 2 = 2, fica do mesmo jeito 
+
+5 / 2 = 2.5, que é o valor real 
+
+para elevar um valor a outro:
+2 ** 8 = 256
+
+type(variavel) = vai me dizer o tipo de dado que estamos trabalhando
+
+para trabalhar com valores maiores pra melhorar a visualização: 1_000_000, 1_000_000_000
+"""
